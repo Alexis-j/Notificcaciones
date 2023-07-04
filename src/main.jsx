ba8299ee734d88515a58fe8d138e8df65ed7e49f
alexis jimenez castillo <alexis.jcastillo@outlook.com>
2023-07-04 09:31:55 +0200

    ain

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,37 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
+
+
+import '@fontsource/roboto/300.css'
+import '@fontsource/roboto/400.css'
+import '@fontsource/roboto/500.css'
+import '@fontsource/roboto/700.css'
+
 import './index.css'
+import { CssBaseline, ThemeProvider, createTheme } from '@mui/material'
+
+const theme = createTheme({
+  palette: {
+    mode: "light",
+    primary: {
+      main: "#005b8f",
+    },
+    secondary: {
+      main: "#171a21",
+    },
+  },
+});
+
+import { BrowserRouter } from "react-router-dom";
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <ThemeProvider theme={theme}>
+      <BrowserRouter>
+        <CssBaseline />
+          <App />
+      </BrowserRouter>
+    </ThemeProvider>
   </React.StrictMode>,
 )
