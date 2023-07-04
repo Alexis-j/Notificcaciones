d317aefa43f7fff0d9b76ebaf9958ab33babb3bf
alexis jimenez castillo <alexis.jcastillo@outlook.com>
2023-07-04 09:36:58 +0200

    navlistDrawer

---
+++ src/assets/components/navbar/NavListDrawer.jsx
@@ -0,0 +1,28 @@
+import { List, ListItem, ListItemText, ListItemButton } from "@mui/material";
+import { Box } from "@mui/system";
+
+export default function NavListDrawer({ navLinks, NavLink, setOpen }) {
+  return (
+    <Box sx={{ width: 200, bgcolor: "#ffffff" }}>
+      <nav>
+        <List>
+          {navLinks.map((item) => (
+            <ListItem
+              disablePadding
+              key={item.title}
+            >
+              <ListItemButton
+                component={NavLink}
+                to={item.path}
+                onClick={() => setOpen(false)}
+                >
+                {/* <ListItemIcon>{item.icon}</ListItemIcon> */}
+                <ListItemText>{item.title}</ListItemText>
+              </ListItemButton>
+            </ListItem>
+          ))}
+        </List>
+      </nav>
+    </Box>
+  );
+}
