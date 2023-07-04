74069776c14123a2895617c7bfe78d2fba138964
alexis jimenez castillo <alexis.jcastillo@outlook.com>
2023-07-04 12:17:24 +0200

    notistack

--- src/assets/components/view/login.jsx
+++ src/assets/components/view/login.jsx
@@ -1,7 +1,22 @@
+import { Button } from "@mui/material";
+import { useSnackbar, enqueueSnackbar } from "notistack";
+
 export default function Login() {
+
+    const {enqueueSnackbar} = useSnackbar()
+
+    const handleClick = () => {
+      enqueueSnackbar("you are successfully logged in", {variant: "success"})
+    }
   return (
   <>
   <h1>Login</h1>
+  <Button
+    variant="contained"
+    onClick={handleClick}
+  >
+    Login
+  </Button>
   </>
   );
 }
--- src/assets/components/view/register.jsx
+++ src/assets/components/view/register.jsx
@@ -1,7 +1,26 @@
+import { Box, Alert, AlertTitle, Snackbar, Button } from "@mui/material";
+import { useState } from "react";
+
 export default function Login() {
+
+    const [open, setOpen] = useState(false)
+
   return (
   <>
-  <h1>Login</h1>
+  <h1>Register</h1>
+  <Box sx={{display:"grid", gap: "1rem"}}>
+    <Button variant="contained"
+      onClick={() => {
+      setOpen(true);
+      }}
+    >open</Button>
+    <Snackbar open={open} autoHideDuration={1000} onClose={() => setOpen(false)}>
+      <Alert severity="error">
+        <AlertTitle>Error</AlertTitle>
+        This is an error alert — check it out!
+      </Alert>
+    </Snackbar>
+  </Box>
   </>
   );
 }
